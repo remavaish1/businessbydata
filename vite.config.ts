8fc8c95206f2430da9527ce32e382681084e7409
remasureshkumar <42535984+remavaish1@users.noreply.github.com>
2026-01-14 08:55:36 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, ".", "");
   return {
-    base: "/businessbydata/", // ✅ required for GitHub Pages project site
+    base: "/", // ✅ ROOT because you are using businessbydata.co
     server: {
       port: 3000,
       host: "0.0.0.0",
