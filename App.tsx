270b081b99306eddd616c44325cea65ecadbcd3d
rema vaish <42535984+remavaish1@users.noreply.github.com>
2026-01-21 23:57:39 +0100

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -22,7 +22,7 @@ const Header: React.FC = () => {
     { name: "Services", path: "/services" },
     { name: "Portfolio", path: "/portfolio" },
     { name: "About", path: "/about" },
-    { name: "Contact", path: "/contact" },
+   
   ];
 
   return (
